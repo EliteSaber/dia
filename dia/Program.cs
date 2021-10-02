fa157754cf6caeef146206a8547f595587a75e9b
EliteSaber <samoilenko-n-a@mail.ru>
2021-10-02 20:18:03 +0300

    Second commit

--- dia/Program.cs
+++ dia/Program.cs
@@ -7,57 +7,52 @@ namespace dia
         static void Main(string[] args)
         {
             int n;
+            bool isNum;
             do
             {
                 Console.Write("Введите нечетное число:");
-                n = Math.Abs(int.Parse(Console.ReadLine()));
-            } while (n % 2 == 0);
-            int m = n / 2,
-                c = -1;
-            bool nap = true;
-            for(int i = 0; i < n; i++)
+                string num = Console.ReadLine();
+                isNum = int.TryParse(num, out n);
+                if (isNum)
+                {
+                    n = Math.Abs(n);
+                    isNum = n % 2 != 0;
+                    isNum = isNum && n < 80;
+                }
+            } while (!isNum);
+            TwoCycles(n);
+        }
+        static void TwoCycles(int n)
+        {
+            int half = n / 2,
+                center = -1;
+            string s = "";
+            for (int i = 0; i < half + 1; i++)
             {
-                string s = "";
-                if (nap)
-                {
-                    for (int j = 0; j < m; j++)
-                    {
-                        s += " ";
-                    }
-                    m--;
+                for (int j = 0; j < half - i; j++)
+                    s += " ";
+                s += "*";
+                for(int j = 0; j < center; j++)
+                    s += " ";
+                center += 2;
+                if (i > 0)
                     s += "*";
-                    for (int j = 0; j < c; j++)
-                    {
-                        s += " ";
-                    }
-                    if (c > 0)
-                        s += "*";
-                    c += 2;
-                }
-                else
-                {
-                    for (int j = m; j >= 0; j--)
-                    {
-                        s += " ";
-                    }
-                    m++;
-                    s += "*";
-                    for (int j = c; j > 0; j--)
-                    {
-                        s += " ";
-                    }
-                    if (c > 0)
-                        s += "*";
-                    c -= 2;
-                }
-                if (m < 0)
-                {
-                    nap = false;
-                    m = 0;
-                    c -= 4;
-                }
-                Console.WriteLine(s);
+                s += "\r\n";
             }
+            center -= 4;
+            for (int i = half; i > 0; i--)
+            {
+                for (int j = half - i; j >= 0; j--)
+                    s += " ";
+                s += "*";
+                for (int j = center; j > 0; j--)
+                    s += " ";
+                center -= 2;
+                if (i > 1)
+                    s += "*";
+                s += "\r\n";
+            }
+            Console.WriteLine(s);
         }
     }
 }
